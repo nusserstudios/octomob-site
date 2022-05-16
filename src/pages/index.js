46b1ffd1653adf06a3c75a12adbdbc44dda2bf73
elnusserino@protonmail.com <elnusserino>
2022-05-15 19:49:30 -0500

    Updated index.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -28,7 +27,7 @@ const IndexPage = () => (
     </div>
     
     <div class="container mx-auto pt-14 px-4">
-      <div class="flex flex-row text-gray-300 grid grid-cols-2 gap-4">
+      <div class="flex flex-row text-gray-300 grid md:grid-cols-2 grid-cols-1 gap-4">
         <div>
           <StaticImage
             src="../images/made-men-logo.png"
@@ -46,7 +45,7 @@ const IndexPage = () => (
     </div>
 
     <div class="container mx-auto pt-14 px-4">
-      <div class="flex flex-row text-gray-300 grid grid-cols-2 gap-4">
+      <div class="flex flex-row text-gray-300 grid md:grid-cols-2 grid-cols-1 gap-4">
         <div class="self-center">
           <StaticImage
             src="../images/hitmen-logo.png"
@@ -64,9 +63,9 @@ const IndexPage = () => (
     </div>
 
     <div class="container mx-auto pt-14 pb-14 px-4">
-      <div class="flex flex-row text-gray-300 grid grid-cols-2 gap-4">
+      <div class="flex flex-row text-gray-300 grid md:grid-cols-2 grid-cols-1 gap-4">
         <div class="self-center">
-        <StaticImage
+          <StaticImage
             src="../images/events-mob.png"
             alt="OctoMob Event Hosting"
             class="rounded-lg border-solid border-2 border-gray-600"
@@ -80,14 +79,130 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
-    <div class="bg-gradient-to-t from-red-700 to-orange-500">
-      <div class="container mx-auto pt-12 pb-12 flex justify-center items-center">
-          <div>
-            <iframe src="https://player.twitch.tv/?video=1481348381&parent=octomob.xyz" frameborder="0" allowfullscreen="true" scrolling="no" height="915" width="1500"></iframe>
+    <div class="bg-gradient-to-t from-zinc-900 to-black">
+      <div class="container mx-auto py-7">
+        <h2 class="uppercase text-5xl pb-4 font-black text-center align-middle">New Weekly vCasts</h2>
+      </div>
+    </div>
+    <div class="bg-gradient-to-t from-pink-900 to-purple-500">
+      <div class="flex flex-row max-w-screen-2xl mx-auto grid grid-cols-1 gap-4 pt-16 pb-0">
+        <div class="px-4">
+          <StaticImage
+            src="../images/mob-ties-featured.png"
+            alt="OctoMob Event Hosting"
+            class="rounded-lg border-solid border-2 border-white-400"
+          />  
+        </div>
+        <div class="flex mx-auto mt-6">
+          <div class="bg-gradient-to-b from-black to-transparent p-8 border-solid border-2 border-white-400 rounded-lg max-w-screen-xl m-4">
+            <h2 class="uppercase text-5xl pb-4 font-black drop-shadow text-center">Mob Ties AnataCast</h2>
+              <p class="leading-7 text-lg mb-6 text-center">MobTies AnataCast, is OctoMob's new weekly vCast. Come chill with the host lilbobross_ and co-host Mad Matty. We cover everything from crypto, blockchain tech, decentralized finance, NFT's, and much more. We've been building this up and our first episode with DCFGod was a hit! MobTies will be running every week. If you are a buildooor, or would like to be on the show, reach out on Discord. If you're interested in contributing reach out, we would love to work with you. Let us know, as our guest list is filling up rather quickly.</p>
+              <div class="flex items-center">
+                <div class="mx-auto">
+                  <a href="https://www.twitch.tv/videos/1481348381?collection=47fzRHoy7hZWYA" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+                      Watch Now
+                    </span>
+                  </a>
+                </div>
+              </div>
           </div>
+        </div>
+        <div class="p-4 md:mb-16 mb-4">
+          <div class="flex flex-row max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 pt-4">
+            <div class="items-center text-center">
+              <StaticImage
+                src="../images/profile-lil-bob-ross.png"
+                alt="The DonFather"
+                class="rounded-lg border-solid border-2 border-white-400"
+                />  
+              <h2 class="text-center font-light pt-2 text-3xl mt-4">Host</h2>
+              <h3 class="text-center font-extrabold pt-2 mb-4 text-3xl">lilbobross</h3>
+              <a href="https://twitter.com/serbobross" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+                  Follow Now
+                </span>
+              </a>
+            </div>
+            <div class="items-center text-center">
+              <StaticImage
+                src="../images/profile-mad-matty.png"
+                alt="El Nusserino"
+                class="rounded-lg border-solid border-2 border-white-400"
+              /> 
+              <h2 class="text-center font-light pt-2 text-3xl mt-4">Co-Host</h2>
+              <h3 class="text-center font-extrabold pt-2 mb-4 text-3xl">Mad Matty</h3>
+              <a href="https://twitter.com/Mad_matty_" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+                  Follow Now
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
+    <div class="bg-gradient-to-b from-zinc-900 to-black">
+      <div class="container mx-auto pt-9 pb-4">
+        <h2 class="uppercase text-5xl pb-4 font-black text-center align-middle">The OctoMob Team</h2>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="grid md:grid-cols-3 grid-cols-1 gap-4 mx-auto max-w-screen-2xl p-4">
+        <div class="items-center text-center">
+          <StaticImage
+            src="../images/profile-donfather.png"
+            alt="The DonFather"
+            class="rounded-lg border-solid border-2 border-gray-600"
+          />  
+          <h2 class="text-center font-light mt-6 mb-4 text-3xl">Project Lead</h2>
+          <h3 class="text-center font-extrabold mb-6 text-3xl">The DonFather</h3>
+          <a href="https://twitter.com/The_DonFather_" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+              Follow Now
+            </span>
+          </a>
+        </div>
+        <div class="items-center text-center">
+          <StaticImage
+            src="../images/profile-nussy.png"
+            alt="El Nusserino"
+            class="rounded-lg border-solid border-2 border-gray-600"
+          /> 
+          <h2 class="text-center font-light mt-6 mb-4 text-3xl">Artist</h2>
+          <h3 class="text-center font-extrabold mb-6 text-3xl">El Nusserino</h3>
+          <a href="https://twitter.com/elNusserino" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+              Follow Now
+            </span>
+          </a>
+        </div>
+        <div class="items-center text-center">
+          <StaticImage
+            src="../images/profile-noosh.png"
+            alt="Noosh"
+            class="rounded-lg border-solid border-2 border-gray-600"
+          /> 
+          <h2 class="text-center font-light mt-6 mb-4 text-3xl">Dev</h2>
+          <h3 class="text-center font-extrabold mb-6 text-3xl">Noosh</h3>
+          <a href="https://twitter.com/nooshzilla" target="_blank" rel="noreferrer" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-2xl font-bold">
+              Follow Now
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex items-center">
+      <div class="mx-auto pt-8 px-4">
+          <StaticImage
+            src="../images/mob-logo-footer.svg"
+            alt="LFG MoB"
+          />
+      </div>
+    </div>
 
   </Layout>
 )
