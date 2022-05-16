13bc15ceebf755f5499a271abc7d9c80f0b50ca1
elnusserino@protonmail.com <elnusserino>
2022-05-15 20:20:39 -0500

    Added GA trackingId.

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "G-6TDKCQFGQ5.",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
